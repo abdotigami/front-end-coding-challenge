0f086a8b3d1562c69763cbce7fa79873d9cfad2f
abdotigami <abdotigami@gmail.com>
2019-10-21 02:05:33 +0100

    Updates

---
+++ precache-manifest.55b16372aaeb28c73f26e9da728ff81d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26354fa57318405ff0446bece5309300",
+    "url": "/front-end-coding-challenge/index.html"
+  },
+  {
+    "revision": "49cbf5649678683c607b",
+    "url": "/front-end-coding-challenge/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2b4b53fd6b2514d54ecc",
+    "url": "/front-end-coding-challenge/static/js/2.0b8bfe91.chunk.js"
+  },
+  {
+    "revision": "49cbf5649678683c607b",
+    "url": "/front-end-coding-challenge/static/js/main.d0d665b2.chunk.js"
+  },
+  {
+    "revision": "0ab0e837b2ffe7eff127",
+    "url": "/front-end-coding-challenge/static/js/runtime-main.295612a8.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/front-end-coding-challenge/static/media/logo.25bf045c.svg"
+  }
+]);
